6022e155b24e38198cecb38a3190f3d02b794bdc
ryokatsu <ryokatsu0719@gmail.com>
2022-08-23 16:04:16 +0900

    feat:tailwindの追加

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <nuxt-img src="/51Z170X4KKL._AC_SY445_.jpg" />
+    <p class="m-10 text-xl underline">Hello World!</p>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/image-edge',
-  ]
-})
+  // 追加
+  css: ["@/assets/css/tailwind.css"],
+  modules: ["@nuxt/image-edge", "@nuxtjs/tailwindcss"],
+
+  // 追加
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
